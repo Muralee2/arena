79d698b470fe6692791ddf023e66c467f7977e8d
Muralee2 <muralimvkaws1@gmail.com>
2025-08-18 00:10:39 +0530

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,14 +2,15 @@ output "network_name" {
   value = google_compute_network.vpc.name
 }
 
-output "subnet_name" {
-  value      = google_compute_subnetwork.subnet.name
-}
-
 output "project_id" {
   value = var.project_id
 }
 
-output "region" {
-  value = var.region
+output "subnet_name" {
+  value = google_compute_subnetwork.subnet.name
 }
+
+output "subnet_region" {
+  value = google_compute_subnetwork.subnet.region
+}
+
