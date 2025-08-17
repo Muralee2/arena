51f626de7e9463cb7ac2f0d312813b296f1b53b9
Muralee2 <muralimvkaws1@gmail.com>
2025-08-17 19:21:50 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -4,7 +4,7 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name                     = var.subnet_name
+  name                     = google_compute_subnetwork.subnet.name
   ip_cidr_range            = var.subnet_cidr
   region                   = var.region
   network                  = google_compute_network.vpc.name
